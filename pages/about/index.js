b8d1ec15c633ec877882579c3c6954c80a30b7b1
Damon Roberts <damon@weekendr.eu>
2021-07-18 19:26:13 +0200

    feat-seed the about and contact pages

--- pages/about/index.js
+++ pages/about/index.js
@@ -1,10 +1,29 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import MainLayout from '../../components/MainLayout';
+import { axiosAirTable } from '../../public/Axios';
 
-function About(props) {
-  return <div>About</div>;
+function About({ paintings, footerLinks }) {
+  return <MainLayout footerLinks={footerLinks}>About</MainLayout>;
 }
 
 About.propTypes = {};
 
 export default About;
+
+export async function getServerSideProps(context) {
+  const paintings = await axiosAirTable
+    .get('/appX2ycFLtWmCcHVb/Paintings')
+    .then((res) => res.data.records);
+
+  const footerLinks = await axiosAirTable
+    .get('/appX2ycFLtWmCcHVb/Footer')
+    .then((res) => res.data.records);
+
+  return {
+    props: {
+      paintings,
+      footerLinks,
+    },
+  };
+}
--- pages/contact/index.js
+++ pages/contact/index.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import MainLayout from '../../components/MainLayout';
+import { axiosAirTable } from '../../public/Axios';
 
-function Contact(props) {
-  return <div>Contact</div>;
+function Contact({ footerLinks }) {
+  return <MainLayout footerLinks={footerLinks}>Contact</MainLayout>;
 }
 
 Contact.propTypes = {};
 
 export default Contact;
+
+export async function getServerSideProps(context) {
+  const footerLinks = await axiosAirTable
+    .get('/appX2ycFLtWmCcHVb/Footer')
+    .then((res) => res.data.records);
+
+  return {
+    props: {
+      footerLinks,
+    },
+  };
+}
